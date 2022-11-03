162b7d7052769e1ae38fd387f11fb05be446996d
jackorofrobert <71209599+jackorofrobert@users.noreply.github.com>
2022-11-03 11:04:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,36 @@ overlay.addEventListener("click", () => {
   menuButton.classList.toggle("open");
   overlay.classList.toggle("show");
 });
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+const observer = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+      if (entry.isIntersecting) {
+          // Add the animation class
+          entry.target.classList.add('skill-pers');
+          return
+      }
+      
+  });
+});
+for (let i = 0; i < document.querySelectorAll('.skill-per').length; i++) {
+  observer.observe(document.querySelectorAll('.skill-per')[i]);;
+}
